54c5859b7029acf0d3d4bee2c4eac2ad6f5324c1
Lukas Kneringer <lukas.kneringer@alturos.com>
2019-04-17 09:19:21 +0200

    Position Converter
    Abstraction and improvement of position converter + Unit tests

---
+++ src/Alturos.PanTilt.UnitTest/EneoConvertTest.cs
@@ -0,0 +1,42 @@
+﻿using Alturos.PanTilt.Eneo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Alturos.PanTilt.UnitTest
+{
+    [TestClass]
+    public class EneoConvertTest
+    {
+        [TestMethod]
+        public void RoundingTestEqual1()
+        {
+            var positionConverter = new PositionConverter();
+            positionConverter.ConvertPositionData(0, out var data1, out var data2);
+            positionConverter.ConvertPositionData(6e-16, out var data3, out var data4);
+
+            Assert.AreEqual(data1, data3);
+            Assert.AreEqual(data2, data4);
+        }
+
+        [TestMethod]
+        public void RoundingTestEqual2()
+        {
+            var positionConverter = new PositionConverter();
+            positionConverter.ConvertPositionData(0, out var data1, out var data2);
+            positionConverter.ConvertPositionData(0.001, out var data3, out var data4);
+
+            Assert.AreEqual(data1, data3);
+            Assert.AreEqual(data2, data4);
+        }
+
+        [TestMethod]
+        public void RoundingTestNotEqual()
+        {
+            var positionConverter = new PositionConverter();
+            positionConverter.ConvertPositionData(0, out var data1, out var data2);
+            positionConverter.ConvertPositionData(0.01, out var data3, out var data4);
+
+            Assert.AreNotEqual(data1, data3);
+            Assert.AreNotEqual(data2, data4);
+        }
+    }
+}
---
+++ src/Alturos.PanTilt/Eneo/PositionConverter.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Alturos.PanTilt.Eneo
+{
+    public class PositionConverter
+    {
+        public void ConvertPositionData(double position, out byte data1, out byte data2)
+        {
+            //To be able to encode two decimal points
+            var targetPosition = (int)(position * 100);
+
+            if (targetPosition == 0)
+            {
+                //Set angle position zero
+                data1 = 0xDA;
+                data2 = 0xAA;
+                return;
+            }
+
+            if (targetPosition > 0)
+            {
+                data1 = (byte)(targetPosition >> 8);
+                data2 = (byte)(targetPosition & 0x00FF);
+
+                if (data1 == 0x00)
+                {
+                    data1 = 0x50;
+                }
+
+                if (data2 == 0x00)
+                {
+                    //Dont move the order it's important first data2
+                    data2 = data1;
+                    data1 = 0x51;
+                }
+
+                return;
+            }
+
+            //position < 0
+            data1 = (byte)(Math.Abs(targetPosition) >> 8);
+            data1 |= 0x80;
+
+            data2 = (byte)(Math.Abs(targetPosition) & 0x00FF);
+
+            if (data1 == 0x00)
+            {
+                data1 = 0xD0;
+            }
+
+            if (data2 == 0x00)
+            {
+                //Dont move the order it's important first data2
+                data2 = data1;
+                data1 = 0xD1;
+            }
+        }
+    }
+}
