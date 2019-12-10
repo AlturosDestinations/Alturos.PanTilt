67b6cc7049adf94fa71c522d98d4888b9d426b79
Tino Hager <tino.hager@alturos.com>
2019-12-10 11:24:25 +0100

    optimize flood test

--- src/Alturos.PanTilt.TestUI/CustomControl/MovementFloodControl.cs
+++ src/Alturos.PanTilt.TestUI/CustomControl/MovementFloodControl.cs
@@ -31,39 +31,37 @@ namespace Alturos.PanTilt.TestUI.CustomControl
         private async void buttonStart_Click(object sender, EventArgs e)
         {
             this.buttonStart.Enabled = false;
+            this.textBoxHistory.Text = string.Empty;
 
             double.TryParse(this.textBoxPan.Text, out var pan);
             double.TryParse(this.textBoxSpeed.Text, out var degreePerSecond);
 
-            this.textBoxHistory.Text = string.Empty;
+            this.textBoxHistory.Invoke(o => o.Text += $"Drive to start position {pan}, please wait...\r\n");
+
             this._panTiltControl.PanTiltAbsolute(pan, 0);
             await this._positionChecker.ComparePositionAsync(new PanTiltPosition(pan, 0), retry: 20);
 
-            this._panTiltControl.PanRelative(degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative {degreePerSecond}\r\n");
-            await Task.Delay(20);
-            this._panTiltControl.PanRelative(degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative {degreePerSecond}\r\n");
-            await Task.Delay(20);
-            this._panTiltControl.PanRelative(degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative {degreePerSecond}\r\n");
-            await Task.Delay(20);
-            this._panTiltControl.PanRelative(degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative {degreePerSecond}\r\n");
-            await Task.Delay(20);
+            this.textBoxHistory.Invoke(o => o.Text += $"Start position reached\r\n");
+            await Task.Delay(100);
 
-            this._panTiltControl.PanRelative(-degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative -{degreePerSecond}\r\n");
-            await Task.Delay(20);
-            this._panTiltControl.PanRelative(-degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative -{degreePerSecond}\r\n");
-            await Task.Delay(20);
-            this._panTiltControl.PanRelative(-degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative -{degreePerSecond}\r\n");
-            await Task.Delay(20);
-            this._panTiltControl.PanRelative(-degreePerSecond);
-            this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative -{degreePerSecond}\r\n");
-            await Task.Delay(20);
+            for (var delay = 0; delay < 200; delay += 10)
+            {
+                for (var i = 0; i < 10; i++)
+                {
+                    this._panTiltControl.PanRelative(degreePerSecond);
+                    this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative {degreePerSecond}\r\n");
+                    await Task.Delay(delay);
+                }
+
+                for (var i = 0; i < 10; i++)
+                {
+                    this._panTiltControl.PanRelative(-degreePerSecond);
+                    this.textBoxHistory.Invoke(o => o.Text += $"{DateTime.Now:HH:mm:ss.fff} PanRelative -{degreePerSecond}\r\n");
+                    await Task.Delay(delay);
+                }
+            }
+
+            this._panTiltControl.StopMoving();
 
             this.buttonStart.Enabled = true;
         }
