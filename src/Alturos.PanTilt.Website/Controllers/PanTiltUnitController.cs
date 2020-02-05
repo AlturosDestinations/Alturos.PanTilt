9dca203f37ee062ee21230e4b099787f6cd7afe4
Tino Hager <tino.hager@nager.at>
2020-02-05 21:18:33 +0100

    Add Website Project

---
+++ src/Alturos.PanTilt.Website/Controllers/PanTiltUnitController.cs
@@ -0,0 +1,64 @@
+﻿using Alturos.PanTilt.Communication;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Alturos.PanTilt.Website.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PanTiltUnitController : ControllerBase
+    {
+        private readonly ILogger<PanTiltUnitController> _logger;
+
+        public PanTiltUnitController(ILogger<PanTiltUnitController> logger)
+        {
+            this._logger = logger;
+        }
+
+        [HttpPost]
+        [Route("MoveToPosition")]
+        public ActionResult MoveToPosition([FromQuery] string ipAddress, [FromBody] PanTiltPosition position)
+        {
+            this._logger.LogDebug($"MoveToPosition {ipAddress} {position}");
+            using (ICommunication communication = new UdpNetworkCommunication(IPAddress.Parse(ipAddress), 5555))
+            using (IPanTiltControl panTiltControl = new AlturosPanTiltControl(communication))
+            {
+                if (panTiltControl.PanTiltAbsolute(position))
+                {
+                    return StatusCode(StatusCodes.Status200OK);
+                }
+
+                return StatusCode(StatusCodes.Status400BadRequest);
+            }
+        }
+
+        [HttpGet]
+        [Route("GetCurrentPosition")]
+        public async Task<ActionResult<PanTiltPosition>> GetCurrentPositionAsync([FromQuery] string ipAddress)
+        {
+            this._logger.LogDebug($"GetCurrentPosition {ipAddress}");
+            using (ICommunication communication = new UdpNetworkCommunication(IPAddress.Parse(ipAddress), 5555))
+            using (IPanTiltControl panTiltControl = new AlturosPanTiltControl(communication))
+            {
+                if (!panTiltControl.Start())
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+
+                await Task.Delay(200);
+
+                var position = panTiltControl.GetPosition();
+
+                if (!panTiltControl.Stop())
+                {
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                }
+
+                return StatusCode(StatusCodes.Status200OK, position);
+            }
+        }
+    }
+}
---
+++ src/Alturos.PanTilt.Website/Startup.cs
@@ -0,0 +1,57 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace Alturos.PanTilt.Website
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1.0", new OpenApiInfo { Title = "Alturos.PanTilt API", Version = "v1.0" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1.0/swagger.json", "Alturos.PanTilt API V1");
+            });
+        }
+    }
+}
